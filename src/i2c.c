bc9c30e62e01776c46d6fdeb4599127987771552
pvvx <pvvx@mail.ru>
2022-04-29 06:21:00 +0300

    test https://github.com/pvvx/ATC_MiThermometer/issues/206 i2c low speed

--- src/i2c.c
+++ src/i2c.c
@@ -8,7 +8,11 @@
 _attribute_ram_code_ void init_i2c(){
 	i2c_gpio_set(I2C_GROUP); // I2C_GPIO_GROUP_C0C1, I2C_GPIO_GROUP_C2C3, I2C_GPIO_GROUP_B6D7, I2C_GPIO_GROUP_A3A4
 	// i2c_master_init(0x78, (uint8_t)(CLOCK_SYS_CLOCK_HZ/(4*750000)) );
+#if 0 // https://github.com/pvvx/ATC_MiThermometer/issues/206
 	reg_i2c_speed = (uint8_t)(CLOCK_SYS_CLOCK_HZ/(4*750000)); //i2c clock = system_clock/(4*DivClock)
+#else
+	reg_i2c_speed = (uint8_t)(CLOCK_SYS_CLOCK_HZ/(4*600000)); //i2c clock = system_clock/(4*DivClock)
+#endif
     //reg_i2c_id  = ?; //slave address
     reg_i2c_mode |= FLD_I2C_MASTER_EN; //enable master mode
 	reg_i2c_mode &= ~FLD_I2C_HOLD_MASTER; // Disable clock stretching for Sensor
