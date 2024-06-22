f3d2df51f09d954ec8c17fe46a9921072308f216
pvvx <pvvx@mail.ru>
2024-06-22 23:19:23 +0300

    Bad https://github.com/pvvx/ATC_MiThermometer/pull/524

--- src/battery.c
+++ src/battery.c
@@ -88,8 +88,8 @@ _attribute_ram_code_
 uint8_t get_battery_level(uint16_t battery_mv) {
 	uint8_t battery_level = 0;
 	if (battery_mv > MIN_VBAT_MV) {
-		battery_level = ((battery_mv - MIN_VBAT_MV) * 100) /
-				(MAX_VBAT_MV - MIN_VBAT_MV);
+		battery_level = (battery_mv - MIN_VBAT_MV) / ((MAX_VBAT_MV
+				- MIN_VBAT_MV) / 100);
 		if (battery_level > 100)
 			battery_level = 100;
 	}
