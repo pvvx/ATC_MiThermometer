ded76ec9fb8c9326367b9daab13241689ccccc6a
Victor <pvvx@mail.ru>
2025-10-13 12:13:44 +0300

    Add CHT8215 to ZTH01

--- src/app_config.h
+++ src/app_config.h
@@ -1131,7 +1131,7 @@ extern "C" {
 #define USE_EPD				0 // min update time ms
 
 #define USE_SENSOR_CHT8305		1
-#define USE_SENSOR_CHT8215		0
+#define USE_SENSOR_CHT8215		1
 #define USE_SENSOR_AHT20_30		1
 #define USE_SENSOR_SHT4X		1
 #define USE_SENSOR_SHTC3		1
